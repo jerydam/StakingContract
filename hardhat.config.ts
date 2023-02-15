be143c0ed8b15f51d8e399c7905978c0ba792827
Jerydam <jerydan148@gmail.com>
2023-02-15 16:14:57 +0100

    mad oo

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
+import dotenv from "dotenv";
+dotenv.config();
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-};
+  solidity: "0.8.9",
 
+  networks: {
+    hardhat: {
+    },
+    goerli: {
+      url: process.env.GOERLI_RPC,
+      //@ts-ignore
+      accounts:[process.env.PRIVATE_KEY1,PRIVATE_KEY,PRIVATE_KEY3 ]},
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  }
+};
 export default config;
